a33c18c53d1bcee565a927caa09dacc0d2f2792b
Daniel Gonzalez <daniel.gonzalez.guijarro@gmail.com>
2020-08-05 19:46:17 +0200

    PRACTICA DE CADENAS

---
+++ NVL-JS-P8a.js
@@ -0,0 +1,11 @@
+var a ="Amigo";
+
+//primera forma
+alert(a.toLowerCase());
+
+//segunda forma
+alert(a.replace("A","a"));
+
+//tercera forma
+a[0]="a"; //Cambio el valor del primer string a "a"
+alert(a); //imprimo el resultado en pantalla
